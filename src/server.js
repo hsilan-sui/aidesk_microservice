5e305e885d9ce58e674999e5a45ec8e1ec97309e
hsilan-sui <suihsilan1101@icloud.com>
2025-05-09 12:03:57 +0800

    chore: 建置好image，成功啟動容器與測試websocket
    - Docker + Redis + Node.js 容器建置
    
    - src/server.js 啟動 Express + Socket.IO，基本伺服器架好

---
+++ src/server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+//🔐 雙向交握（Handshake）
+// 測試連線
+io.on("connection", (socket) => {
+  console.log(`${socket.id} 使用者已經連線！！！`);
+
+  socket.on("client:message", (msg) => {
+    console.log(`收到來自 ${socket.id} 的訊息：${msg}`);
+
+    //假裝ai回覆
+    socket.emit(
+      "ai:reply",
+      `森森不息AI回覆：嗨！你說的是${msg}嗎？ 系統維護中，請稍後再試！`
+    );
+  });
+
+  // 處理離線事件
+  socket.on("disconnect", (reason) => {
+    console.log(`使用者 ${socket.id} 已離線：${reason}`);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("AI 客服伺服器正在運行中...");
+});
+
+server.listen(process.env.PORT || 5005, () => {
+  console.log(`伺服器啟動於 port ${process.env.PORT || 5005}`);
+});
